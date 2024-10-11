42478538c2f7431b48e344b3b2740cc690896047
GideonAmhaG <gideonamha@gmail.com>
2024-10-11 16:53:33 +0300

    can now show user posts in dash board

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -36,10 +36,10 @@ export const getPosts = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
     const posts = await Post.find({
-      ...(req.query.userId && [{ userId: req.query.userId }]),
-      ...(req.query.category && [{ category: req.query.category }]),
-      ...(req.query.slug && [{ slug: req.query.slug }]),
-      ...(req.query.postId && [{ _id: req.query.postId }]),
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
           { title: { $regex: req.query.searchTerm, $options: "i" } },
@@ -50,17 +50,26 @@ export const getPosts = async (req, res, next) => {
       .sort({ updatedAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
+
     const totalPosts = await Post.countDocuments();
+
     const now = new Date();
+
     const oneMonthAgo = new Date(
       now.getFullYear(),
       now.getMonth() - 1,
       now.getDate()
     );
+
     const lastMonthPosts = await Post.countDocuments({
       createdAt: { $gte: oneMonthAgo },
     });
-    res.status(200).json({ posts, totalPosts, lastMonthPosts });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
   } catch (error) {
     next(error);
   }
