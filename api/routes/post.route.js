62e4141b98cf2e5ff741cba71e4dc40fa6de34ec
GideonAmhaG <gideonamha@gmail.com>
2024-10-14 12:45:21 +0300

    added update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,6 +4,7 @@ import {
   create,
   getPosts,
   deletePost,
+  updatePost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyUser, create);
 router.get("/getposts", getPosts);
 router.delete("/deletepost/:postId/:userId", verifyUser, deletePost);
+router.put("/updatepost/:postId/:userId", verifyUser, updatePost);
 
 export default router;
