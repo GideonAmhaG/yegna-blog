76ce1f35df97b07d823f9b38c059a2bf3108776c
GideonAmhaG <gideonamha@gmail.com>
2024-11-15 18:30:01 +0300

    Added comments section to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -6,6 +6,7 @@ import {
   likeComment,
   editComment,
   deleteComment,
+  getComments,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
@@ -15,5 +16,6 @@ router.get("/getpostcomments/:postId", getPostComments);
 router.put("/likecomment/:commentId", verifyUser, likeComment);
 router.put("/editcomment/:commentId", verifyUser, editComment);
 router.delete("/deletecomment/:commentId", verifyUser, deleteComment);
+router.get("/getcomments", verifyUser, getComments);
 
 export default router;
